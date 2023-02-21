536d3e3d5a6b90239e2317c8c9813a9c21f60365
alxglzgmz <115950183+alxglzgmz@users.noreply.github.com>
2023-02-21 18:19:32 +0100

    Create pandas.py
    Pruebas, no se bien que hago pero funciona

---
+++ pandas.py
@@ -0,0 +1,46 @@
+import sqlite3
+import pandas as pd
+
+# def sql_update(con):
+#     cursorObj = con.cursor()
+#     cursorObj.execute('UPDATE usuarios SET nombre = "Sergio" where dni = "X"')
+#     con.commit()
+#
+def sql_fetch(con):
+    cursorObj = con.cursor()
+    cursorObj.execute('SELECT * FROM alerts')
+    #SELECT dni, nombre FROM usuarios WHERE altura > 1.0
+    rows = cursorObj.fetchall()
+    for row in rows:
+        print(row)
+#
+def sql_delete(con):
+    cursorObj = con.cursor()
+    cursorObj.execute('DROP TABLE alerts')
+    con.commit()
+#
+# def sql_delete_table(con):
+#     cursorObj = con.cursor()
+#     cursorObj.execute('drop table if exists usuarios')
+#     con.commit()
+
+def sql_create_table(con):
+    cursorObj = con.cursor()
+    cursorObj.execute('''CREATE TABLE alerts (timestampa text ,sid text ,msg text ,clasificacion text ,prioridad int,protocolo text,origen text ,destino text ,puerto int)''')
+    con.commit()
+
+con = sqlite3.connect('example.db')
+sql_delete(con)
+sql_create_table(con)
+
+alerts = pd.read_csv('alerts.csv')
+alerts.to_sql('alerts', con, if_exists='append', index=False)
+
+
+sql_fetch(con)
+# sql_update(con)
+# sql_fetch(con)
+
+# sql_fetch(con)
+# sql_delete_table(con)
+# con.close()
