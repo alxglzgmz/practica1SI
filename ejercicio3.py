930230ae48aa5f148682b4d6dbe11ac108489a5a
alxglzgmz <115950183+alxglzgmz@users.noreply.github.com>
2023-02-28 19:05:24 +0100

    Cambios para devices

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,46 +1,30 @@
+import json
 import sqlite3
 import pandas as pd
 
 
-# def sql_update(con):
-#     cursorObj = con.cursor()
-#     cursorObj.execute('UPDATE usuarios SET nombre = "Sergio" where dni = "X"')
-#     con.commit()
-#
-def sql_fetch(con):
-    cursorObj = con.cursor()
-    cursorObj.execute('SELECT * FROM alerts')
-    rows = cursorObj.fetchall()
-    for row in rows:
-        print(row)
-#
-def sql_delete_table(con):
-    cursorObj = con.cursor()
-    cursorObj.execute('DROP TABLE alerts')
-    con.commit()
 
 
-def sql_create_table(con, dev):
+
+def funcionPrincipal(con):
     cursorObj = con.cursor()
     cursorObj.execute('''CREATE TABLE alerts (timestampa text ,sid text ,msg text ,clasificacion text ,prioridad int,protocolo text,origen text ,destino text ,puerto int)''')
-    cursorObj.execute('''CREATE TABLE devices(id text, ip text, localizacion text, responsable ''')
+    cursorObj.execute('''CREATE TABLE devices(id text PRIMARY KEY, ip text, localizacion text, ''')
+    cursorObj.execute('''CREATE TABLE responsable(id text PRIMARY KEY, nombre text, telefono text, rol text)''')
+    cursorObj.execute('''CREATE TABLE analisis(id INTEGER PRIMARY KEY, puertosabiertos text, ''' )
     con.commit()
 
 
-def sql_count_distinct(con):
-    cursorObj = con.cursor()
-    cursorObj.execute('SELECT COUNT(DISTINCT sid) from alerts')
-    num = cursorObj.fetchone()
-    print("El número distinto de dispositivos es: " + str(num[0]))
+
 
 def sql_count_alerts(con):
     cursorObj = con.cursor()
-    cursorObj.execute('SELECT COUNT(msg) from alerts')
+    cursorObj.execute('SELECT COUNT(*) from alerts')
     num = cursorObj.fetchone()
     print("El número de alertas es: " + str(num[0]))
 
 con = sqlite3.connect('example.db')
-sql_delete_table(con)
+
 sql_create_table(con)
 df = pd.read_json('devices.json')
 dev = sqlite3.connect('devices.db')
@@ -48,6 +32,9 @@ dev = sqlite3.connect('devices.db')
 
 alerts = pd.read_csv('alerts.csv')
 alerts.to_sql('alerts', con, if_exists='append', index=False)
+
+f = open('devices.json','r')
+devices = json.load(f)
 
 
 
