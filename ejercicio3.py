8c18fa3550f92d1ac3a48746a2997ca958a49496
alxglzgmz <115950183+alxglzgmz@users.noreply.github.com>
2023-03-02 15:54:18 +0100

    Tablas Responsable y Analisis creadas con datos insertados.

--- ejercicio3.py
+++ ejercicio3.py
@@ -6,13 +6,47 @@ import pandas as pd
 
 
 
-def funcionPrincipal(con):
+def creacionTablas(con):
     cursorObj = con.cursor()
-    cursorObj.execute('''CREATE TABLE alerts (timestampa text ,sid text ,msg text ,clasificacion text ,prioridad int,protocolo text,origen text ,destino text ,puerto int)''')
-    cursorObj.execute('''CREATE TABLE devices(id text PRIMARY KEY, ip text, localizacion text, ''')
-    cursorObj.execute('''CREATE TABLE responsable(id text PRIMARY KEY, nombre text, telefono text, rol text)''')
-    cursorObj.execute('''CREATE TABLE analisis(id INTEGER PRIMARY KEY, puertosabiertos text, ''' )
+
+    cursorObj.execute('''DROP TABLE IF EXISTS alerts''')
+    cursorObj.execute('''DROP TABLE IF EXISTS analisis''')
+    cursorObj.execute('''DROP TABLE IF EXISTS responsable''')
+    cursorObj.execute('''DROP TABLE IF EXISTS devices''')
+
+
+    cursorObj.execute('''CREATE TABLE IF NOT EXISTS alerts (timestampa text ,sid text ,msg text ,clasificacion text ,prioridad int,protocolo text,origen text ,destino text ,puerto int)''')
+    cursorObj.execute('''CREATE TABLE IF NOT EXISTS analisis(id INTEGER PRIMARY KEY, puertosabiertos text, numberports int, servicios int, servicios_inseguros int, vulnerabilidades int)''')
+    cursorObj.execute('''CREATE TABLE IF NOT EXISTS responsable(nombre text PRIMARY KEY, telefono text, rol text)''')
+    cursorObj.execute('''CREATE TABLE IF NOT EXISTS devices(id text PRIMARY KEY, ip text, localizacion text, nombre_responsable text, analisis_id int, FOREIGN KEY(nombre_responsable) REFERENCES responsable(nombre), FOREIGN KEY(analisis_id) REFERENCES analisis(id))''')
+
+    cursorObj.execute('''INSERT INTO responsable VALUES ("admin", "656445552","Administracion de sistemas") ON CONFLICT(nombre) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO responsable VALUES ("Paco Garcia", "640220120","Direccion") ON CONFLICT(nombre) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO responsable VALUES ("Luis Sanchez", "None","Desarrollador") ON CONFLICT(nombre) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO responsable VALUES ("admin", "656445552","Administracion de sistemas") ON CONFLICT(nombre) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO responsable VALUES ("admiin", "None","None") ON CONFLICT(nombre) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO responsable VALUES ("admin", "656445552","Administracion de sistemas") ON CONFLICT(nombre) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO responsable VALUES ("admin", "656445552","Administracion de sistemas") ON CONFLICT(nombre) DO NOTHING''')
+    rowsResponsable = cursorObj.execute('''SELECT * FROM responsable''').fetchall()
+    for rows in rowsResponsable:
+        print(rows)
+
+    cursorObj.execute('''INSERT INTO analisis VALUES(1,"80/TCP, 443/TCP, 3306/TCP, 40000/UDP",4,3,0,15) ON CONFLICT(id) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO analisis VALUES(2,"None",0,0,0,4) ON CONFLICT(id) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO analisis VALUES(3,"1194/UDP, 8080/TCP, 8080/UDP, 40000/UDP",4,1,1,52) ON CONFLICT(id) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO analisis VALUES(4,"443/UDP, 80/TCP",2,1,0,3) ON CONFLICT(id) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO analisis VALUES(5,"80/TCP, 67/UDP, 68/UDP",3,2,2,12) ON CONFLICT(id) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO analisis VALUES(6,"8080/TCP, 3306/TCP, 3306/UDP",3,2,0,2) ON CONFLICT(id) DO NOTHING''')
+    cursorObj.execute('''INSERT INTO analisis VALUES(7,"80/TCP, 443/TCP, 9200/TCP, 9300/TCP, 5601/TCP",5,3,2,21) ON CONFLICT(id) DO NOTHING''')
+    rowsAnalisis = cursorObj.execute('''SELECT * FROM analisis''').fetchall()
+    for rows in rowsAnalisis:
+        print(rows)
+
+
     con.commit()
+
+
+
 
 
 
@@ -25,7 +59,8 @@ def sql_count_alerts(con):
 
 con = sqlite3.connect('example.db')
 
-sql_create_table(con)
+
+creacionTablas(con)
 df = pd.read_json('devices.json')
 dev = sqlite3.connect('devices.db')
 
@@ -38,5 +73,4 @@ devices = json.load(f)
 
 
 
-sql_count_distinct(con)
-sql_count_alerts(con)
+
